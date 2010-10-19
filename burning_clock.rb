9dd38c2463cf76969dde5466bd6d4f4f95ce6115
Mike Zazaian <mk@mkzzn.org>
2010-10-18 22:54:03 -0400

    completely amend stylesheets, move style to a sass document, remove old css doc, add circumstantial text for multiple day-circumstances

--- burning_clock.rb
+++ burning_clock.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'sinatra'
 require 'haml'
 
@@ -6,6 +5,17 @@ class BurningClock < Sinatra::Application
   set :server, %w[ unicorn thin mongrel ]
 
   get '/' do
+    @days = -1
     haml :index
+  end
+
+  get '/main.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    sass :main
+  end
+
+  protected
+  def days_until_the_burn
+    (Time.gm(2011,9,5) - Time.now).to_i/60/60/24
   end
 end
